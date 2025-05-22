54b7dc0ca450122e6038698fe1ed58ae230e72c2
Fahrur Rozi <fahrurrosi430@gmail.com>
2025-05-22 19:46:29 +0700

    buat halaman pendaftaran alumni

---
+++ resources/views/pendaftaran-alumni.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ini adalah halaman pendaftaran alumni</h1>
+    <p>Form input biodata lulusan</p>
+    <p>Upload CV</p>
+    <p>Pilihan minat kerja</p>
+</body>
+</html>
